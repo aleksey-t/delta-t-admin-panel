bdd174a9f52c93d3931e59c6408deb86d6e68fe1
Urgant <john_h@list.ru>
2022-02-05 21:11:31 +0300

    изменения в странице 'отдельный продукт'

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Nav } from "react-bootstrap";
 
 const SingleProduct=({})=>{
-    return(<h1>Single Product</h1>)
+    return(
+        <div>
+            <h1>Отдельный продукт</h1>
+        <Nav defaultActiveKey="/home" className="flex-column">
+            <Link to="/">На главную</Link>
+            <Link to="/login">Логин</Link>
+            <Link to="/products">Продукция</Link>
+            <Link to="/new-product">Добавить продукт</Link>
+          </Nav>
+    
+            
+        </div>
+    
+    )
 }
 
 export default SingleProduct
