5c2f248e29b65ec234438ce1772caf29f1c75e9f
Urgant <john_h@list.ru>
2022-02-06 21:24:43 +0300

    добавил компонент sidebar, фон ссылкам

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,23 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Nav } from "react-bootstrap";
+import Sidebar from "../components/Sidebar.jsx";
 
-
-const NewProduct=({})=>{
-    return(<>
+const NewProduct = ({ }) => {
+  return (<>
     <h1>Добавить продукт</h1>
     <div>
-    <Nav defaultActiveKey="/home" className="flex-column">
-        <Link to="/">На главную</Link>
-        <Link to="/login">Логин</Link>
-        <Link to="/products">Продукция</Link>
-        <Link to="products/:productId">Отдельный продукт</Link>
-      </Nav>
-
-        
+      <Sidebar />
     </div>
     {/*форма добавления товара*/}
-    </>)
+  </>)
 }
 
-export default NewProduct
+export default NewProduct;
