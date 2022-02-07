bba458201a9d092caf335f1d5bb1b5c9007aeb0c
Urgant <john_h@list.ru>
2022-02-07 22:18:28 +0300

    добавлен Header и Notification

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import "./header.css";
+import React from "react";
+import { Button } from "react-bootstrap";
+import PropTypes from 'prop-types';
+import Notification from "./Notification";
+
+const Header = (props) => {
+  return (
+    <header className="header-bar">
+        <Notification showNotification={true}/>
+      <div className="nick-name">{props.username}</div>
+      <Button variant="primary" size="sm" className="send">
+        Выход
+      </Button>{" "}
+    </header>
+  );
+};
+
+Header.propTypes = {
+  username: PropTypes.string,
+  emailsNumber: PropTypes.number,
+};
+
+
+
+export default Header;
--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Sidebar from "../components/Sidebar.jsx";
+import Header from "../components/Header";
+import { Row, Container, Col } from "react-bootstrap";
 
 const SingleProduct = () => {
   return (
-    <div>
-      <h1>Отдельный продукт</h1>
-      <Sidebar />
-    </div>
+    <Container fluid>
+      <Row>
+        <Sidebar />
+        <Col>
+          <Header username="dima777"/>
+          <h1>Отдельный продукт</h1>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
