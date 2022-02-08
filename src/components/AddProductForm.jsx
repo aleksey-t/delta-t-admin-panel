00aa1b63578a1a018f999ec0976a1713bd7ecec7
Urgant <john_h@list.ru>
2022-02-08 21:08:13 +0300

    поправил форму

--- src/components/AddProductForm.jsx
+++ src/components/AddProductForm.jsx
@@ -3,31 +3,28 @@ import { Form, Col, Row, Button } from "react-bootstrap";
 
 const AddProductForm = () => {
   return (
-    <div>
-      <br />
-      <Row>
-        <Form.Label column lg={1}>
-          Название:
+    <Form>
+      <Form.Group>
+          <Col xs={1}>
+        <Form.Label column>
+          Название
         </Form.Label>
+          </Col>
         <Col xs={6}>
           <Form.Control type="text" placeholder="Название товара" />
         </Col>
-      </Row>
-      <br />
-      <Row>
-        <Form.Label column lg={1}>
+      </Form.Group>
+
+      <Form.Group>
+          <div className="horizontal-label">
+        <Form.Label column>
           Цена:
         </Form.Label>
-        <Col>
-          <Form.Control
-            type="text"
-            className="mb-3"
-            placeholder="Цена товара"
-          />
-        </Col>
-      </Row>
-      <Row>
-        <Form.Label column lg={1}>
+          </div>
+        <Form.Control type="text" className="mb-3" placeholder="Цена товара" />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label column xs={1}>
           Бренд:
         </Form.Label>
         <Form.Select defaultValue="Choose..." className="mb-3">
@@ -36,10 +33,10 @@ const AddProductForm = () => {
           <option>...</option>
           <option>...</option>
         </Form.Select>
-      </Row>
+      </Form.Group>
 
-      <Row>
-        <Form.Label column lg={1}>
+      <Form.Group>
+        <Form.Label column xs={1}>
           Картинка:
         </Form.Label>
         <Col>
@@ -47,9 +44,9 @@ const AddProductForm = () => {
             <Form.Control type="file" />
           </Form.Group>
         </Col>
-      </Row>
+      </Form.Group>
       <Row>
-        <Form.Label column lg={1}>
+        <Form.Label column xs={1}>
           Категория
         </Form.Label>
         <Form.Select defaultValue="Choose..." className="mb-3">
@@ -64,32 +61,27 @@ const AddProductForm = () => {
           <option>...</option>
           <option>...</option>
         </Form.Select>
-        <br />
-        <Row>
-          <Form.Label column lg={1}>
-            Характеристики:
-          </Form.Label>
-          <Col>
-            <Form.Select defaultValue="Choose..." className="mb-3">
-              <option>Выберите</option>
-              <option>...</option>
-              <option>...</option>
-              <option>...</option>
-            </Form.Select>
-          </Col>
-        </Row>
       </Row>
-      <br />
-      <br />
+      <Form.Group>
+        <Form.Label column xs={1}>
+          Характеристики:
+        </Form.Label>
+        <Col>
+          <Form.Select defaultValue="Choose..." className="mb-3">
+            <option>Выберите</option>
+            <option>...</option>
+            <option>...</option>
+            <option>...</option>
+          </Form.Select>
+        </Col>
+      </Form.Group>
       <Form.Group className="mb-13" id="formGridCheckbox">
         <Form.Check type="checkbox" label="Публикация товара" />
       </Form.Group>
-      <br />
-      <Button variant="primary" type="submit">
+      <Button variant="primary" type="submit" className="mt-4">
         Сохранить
       </Button>
-      <br />
-    </div>
+    </Form>
   );
 };
 
