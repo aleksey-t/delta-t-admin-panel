29e1b274657d034ff39bd6cf03821f5bf677b08b
Urgant <john_h@list.ru>
2022-02-13 23:11:28 +0300

    добавил названия брендов и фото

--- src/pages/BrandsPage.jsx
+++ src/pages/BrandsPage.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import data from "../fake-data/brands.json";
-import {Link} from "react-router-dom";
 
 const BrandsPage = () => {
   return (
     <>
       <h1>Brands</h1>
 
-        <h5>Нажмите на бренд, чтобы отредактировать его</h5>
-        {/*todo: помимо name вывести еще и картинку бренда и все это дело завернуть в ссылку из react-router*/}
-      {data.map((i) => (
-        <div key={i.name}>{i.name}</div>
-      ))}
-        {data.map((i) => (
-           <div key={i.image}><Link to="/brands/all">{i.image}</Link></div>
-        ))}
-
+      <h5>Нажмите на бренд, чтобы отредактировать его</h5>
+      {data.map(function (currentArrayElement, currentIndex) {
+        return (
+          <div key={currentIndex}>
+            <h4>{currentArrayElement.name}</h4>
+            <div className="brand-image">
+              <img src={currentArrayElement.image} alt="" />
+            </div>
+          </div>
+        );
+      })}
     </>
   );
 };
