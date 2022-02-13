1fe30a8d1c962cf063e02bdbe3fbb9dc94cebeac
Urgant <john_h@list.ru>
2022-02-13 19:56:27 +0300

    добавил перебор элементов image

--- src/pages/BrandsPage.jsx
+++ src/pages/BrandsPage.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import data from "../fake-data/brands.json";
+import {Link} from "react-router-dom";
 
 const BrandsPage = () => {
   return (
     <>
       <h1>Brands</h1>
 
+        <h5>Нажмите на бренд, чтобы отредактировать его</h5>
         {/*todo: помимо name вывести еще и картинку бренда и все это дело завернуть в ссылку из react-router*/}
       {data.map((i) => (
         <div key={i.name}>{i.name}</div>
       ))}
+        {data.map((i) => (
+           <div key={i.image}><Link to="/brands/all">{i.image}</Link></div>
+        ))}
 
     </>
   );
